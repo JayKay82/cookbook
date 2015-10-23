167cdc3d2d124be6a75c35d97344dc244be0a5b8
Jan Kroezen <jankroezen@me.com>
2015-10-23 18:19:06 +0100

    Convert registrations/new to slim and to a partial, render it from welcome#index, style the form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  private
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  helper_method :resource_name, :resource, :devise_mapping
 end
