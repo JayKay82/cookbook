7fec531a38043436a0f381691bcb1e14730158ad
Jan Kroezen <jankroezen@me.com>
2015-10-29 16:26:48 +0000

    Implement site navigation, make further layout and link tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # User routes
   devise_for :users
+  resources :users, only: [:show]
+
+  # Root routes
   authenticated :user do
     root to: 'recipes#index', as: 'authenticated_root'
   end
   root to: 'welcome#index'
 
+  # Recipe routes
   resources :recipes, only: [:show,:new,:create, :edit, :update] do
     resources :ingredients, only: [:create, :new]
   end
